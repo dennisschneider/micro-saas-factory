c26e10029adf112902e958c6927d98805f005983
Dennis <hello@dennisschneider.de>
2023-05-06 10:42:02 +0200

    feat(turbo): update prettier config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className="m-5 p-5">Web</h1>
       <Button>Button</Button>
     </div>
   );
