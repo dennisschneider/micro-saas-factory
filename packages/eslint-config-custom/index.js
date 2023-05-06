c7fc9bdd30a0a00e4119da5d709ecf62bfb577df
Dennis <hello@dennisschneider.de>
2023-05-06 08:37:57 +0200

    feat(turbo): update eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -8,4 +8,31 @@ module.exports = {
       presets: [require.resolve("next/babel")],
     },
   },
+  extends: [
+    "next/core-web-vitals",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+  ],
+  plugins: ["tailwindcss"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/classnames-order": "error",
+  },
+  settings: {
+    tailwindcss: {
+      callees: ["cn"],
+      config: "tailwind.config.js",
+    },
+    next: {
+      rootDir: true,
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 };
